e01da74517c7348f02a272153fd40ed24042acc0
ar4366 <ar4366@nyu.edu>
2021-04-07 10:37:00 +0400

    Commit includes small dataset and movie lens dataset
    still incomplete. commiting to save prog

--- code/src/run_dynamics_with_rec.py
+++ code/src/run_dynamics_with_rec.py
@@ -15,86 +15,14 @@ from algorithm.cosin import CosinSimilarity
 
 from lenskit.datasets import ML100K
 
-movielens = ML100K("./ml-100k")
-ratings = movielens.ratings
+# movielens = ML100K("./ml-100k")
+# ratings = movielens.ratings
 #ratings.head()
 
 algo_cosin = CosinSimilarity()
 
 algo_cosin.fit(ratings)
-
-
-def get_mu(v1, v2):
-    mu = np.dot(v1,np.transpose(v2))
-    if mu<0.0000000001000000001:
-        mu = 0.0000000001000000001
-    return mu
-
-def get_alpha_beta(mu, sigma=10**(-5)):
-    a1 = ((1-mu)/(sigma**2)-(1/mu))*mu**2
-    b1 = a1*((1/mu)- 1)
-    return a1, b1
-
-def get_Vui(u, i, p, a):
-    global p_curr, a_curr
-    B_mu = get_mu(p[u-1],a[i-1])
-    alpha, beta = get_alpha_beta(B_mu)
-    # if alpha<0 or beta<0:
-    #     print(p[u-1])
-    #     print(a[i-1])
-    #     print(B_mu)
-    #     print(alpha)
-    #     print(beta)
-    #     p_curr = p[u-1]
-    #     a_curr = a[i-1]
-    return float(beta_dist.rvs(alpha, beta))
-
-def get_nui(mu = 0.98):
-    alpha, beta = get_alpha_beta(mu)
-    return float(np.random.beta(alpha,beta))
-
-def conv_index_to_bins(index):
-    """Calculate bins to contain the index values.
-    The start and end bin boundaries are linearly extrapolated from 
-    the two first and last values. The middle bin boundaries are 
-    midpoints.
-
-    Example 1: [0, 1] -> [-0.5, 0.5, 1.5]
-    Example 2: [0, 1, 4] -> [-0.5, 0.5, 2.5, 5.5]
-    Example 3: [4, 1, 0] -> [5.5, 2.5, 0.5, -0.5]"""
-    assert index.is_monotonic_increasing or index.is_monotonic_decreasing
-
-    # the beginning and end values are guessed from first and last two
-    start = index[0] - (index[1]-index[0])/2
-    end = index[-1] + (index[-1]-index[-2])/2
-
-    # the middle values are the midpoints
-    middle = pd.DataFrame({'m1': index[:-1], 'p1': index[1:]})
-    middle = middle['m1'] + (middle['p1']-middle['m1'])/2
-
-    if isinstance(index, pd.DatetimeIndex):
-        idx = pd.DatetimeIndex(middle).union([start,end])
-    elif isinstance(index, (pd.Float64Index,pd.RangeIndex,pd.Int64Index)):
-        idx = pd.Float64Index(middle).union([start,end])
-    else:
-        print('Warning: guessing what to do with index type %s' % 
-              type(index))
-        idx = pd.Float64Index(middle).union([start,end])
-
-    return idx.sort_values(ascending=index.is_monotonic_increasing)
-
-def calc_df_mesh(df):
-    """Calculate the two-dimensional bins to hold the index and 
-    column values."""
-    return np.meshgrid(conv_index_to_bins(df.index),
-                       conv_index_to_bins(df.columns))
-
-def heatmap(df):
-    """Plot a heatmap of the dataframe values using the index and 
-    columns"""
-    X,Y = calc_df_mesh(df)
-    c = plt.pcolormesh(X, Y, df.values.T, cmap = 'gist_yarg')
-    plt.colorbar(c)    
+   
     
 def get_rank_func(recs, func_type = "sigmoid"):
     x = np.array([r for r in range(1,recs + 1,1)])
@@ -104,41 +32,7 @@ def get_rank_func(recs, func_type = "sigmoid"):
         func = 1/(1+math.e**((x-b)/a))
     if func_type == "dec_exp":
         func = x**(-0.8)
-    return func
-
-def initialize_known_utility(num_u, num_i, total_sim_steps, new_i, new_u, alpha_user=1, alpha_item = 100, K_pref_dimensions = 20):
-    
-    
-    n_i = num_i + (total_sim_steps*new_i)
-    n_u = num_u + (total_sim_steps*new_u)
-    
-    col = np.arange(1, n_i+1, 1)
-    empty_data_row = [float(0) for item in range(1, n_i+1)]
-    empty_data = np.array([empty_data_row for item in range(1, n_u+2)])
-    
-    V_df = pd.DataFrame(empty_data, columns = col)
-    P_df = pd.DataFrame(empty_data, columns = col)
-    V_df = V_df.drop([0], axis = 0)
-    P_df = P_df.drop([0], axis = 0)
-    
-    
-    u_p = dirichlet.rvs([alpha_user]*K_pref_dimensions, size=1)
-    u_a = dirichlet.rvs([alpha_item]*K_pref_dimensions, size=1)
-    
-    p = dirichlet.rvs(u_p[0]*10, size=n_u)
-    a = dirichlet.rvs(u_a[0]*0.1, size=n_i)
-    
-    for item in range(1,n_i+1):
-        for user in range(1, n_u+1):
-            Vui = get_Vui(user,item, p, a)
-            V_df[item][user] = Vui
-            known_proportion = get_nui()
-            P_df[item][user] = Vui*known_proportion
-            
-    return P_df
-            
-
-
+    return func            
 
 def env_step(P_df, prob_explore,num_rec):
     inter_users=[]
@@ -187,22 +81,22 @@ def add_interactions(list_of_interactions):
 
 
 
-num_u = len(movielens.users)
+num_u = 200
 # num_u = 5
-num_i = len(movielens.movies)
+num_i = 400
 # num_i = 5
 num_rec = 30
 simulation_steps = 100
-new_i = 10
-new_u = 10
+new_i = 2
+new_u = 2
 
 
-# Intializing teh full utilities matrix
-StartTime = datetime.now()
-print("Initizing Full Utility Matrix.......")
-Full_Known_Utilities = initialize_known_utility(num_u, num_i, simulation_steps, new_i, new_u)
-print("Initization of Full Utility Matrix Complete")
-print("Time taken: ", datetime.now() - StartTime)
+# # Intializing teh full utilities matrix
+# StartTime = datetime.now()
+# print("Initizing Full Utility Matrix.......")
+# Full_Known_Utilities = initialize_known_utility(num_u, num_i, simulation_steps, new_i, new_u)
+# print("Initization of Full Utility Matrix Complete")
+# print("Time taken: ", datetime.now() - StartTime)
 
 
 #storing max utilities by user for thresholding
